820df1e350d785f1d6d882256603a4100fa33b8d
Ouidad7 <widadelsouri@gmail.com>
2026-03-31 20:28:19 +0100

    update

--- app.py
+++ app.py
@@ -1,18 +1,92 @@
-from flask import Flask
-from database.db import init_db
+from flask import Flask, render_template, request, redirect, jsonify
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key = "lifeos_secret"
 
-init_db()
 
-from routes.auth_routes import auth
-from routes.task_routes import tasks
-from routes.notes_routes import notes
+def get_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
-app.register_blueprint(auth)
-app.register_blueprint(tasks)
-app.register_blueprint(notes)
+
+def init_db():
+
+    db = get_db()
+
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        status TEXT NOT NULL DEFAULT 'todo'
+    )
+    """)
+
+    db.commit()
+
+
+@app.route("/")
+def home():
+    return redirect("/dashboard")
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    db = get_db()
+
+    tasks = db.execute("SELECT * FROM tasks").fetchall()
+
+    return render_template("dashboard.html", tasks=tasks)
+
+
+@app.route("/tasks")
+def tasks():
+
+    db = get_db()
+
+    tasks = db.execute("SELECT * FROM tasks").fetchall()
+
+    return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/add_task", methods=["POST"])
+def add_task():
+
+    title = request.form["title"]
+
+    db = get_db()
+
+    db.execute(
+        "INSERT INTO tasks (title, status) VALUES (?, 'todo')",
+        (title,)
+    )
+
+    db.commit()
+
+    return redirect("/tasks")
+
+
+@app.route("/update_task", methods=["POST"])
+def update_task():
+
+    data = request.get_json()
+
+    task_id = data["task_id"]
+    status = data["status"]
+
+    db = get_db()
+
+    db.execute(
+        "UPDATE tasks SET status=? WHERE id=?",
+        (status, task_id)
+    )
+
+    db.commit()
+
+    return jsonify({"success": True})
+
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,40 @@
-function allowDrop(ev) {
+function allowDrop(ev){
 ev.preventDefault()
 }
 
-function drag(ev) {
-ev.dataTransfer.setData("text", ev.target.id)
+function drag(ev){
+ev.dataTransfer.setData("text",ev.target.id)
 }
 
-function drop(ev) {
+function drop(ev){
 
 ev.preventDefault()
 
 const taskId = ev.dataTransfer.getData("text")
+
 const taskElement = document.getElementById(taskId)
 
-ev.target.closest(".column").appendChild(taskElement)
+const column = ev.target.closest(".column")
 
-const newStatus = ev.target.closest(".column").dataset.status
+column.appendChild(taskElement)
+
+const newStatus = column.dataset.status
 
 const id = taskId.split("-")[1]
 
 fetch("/update_task",{
+
 method:"POST",
+
 headers:{
 "Content-Type":"application/json"
 },
+
 body:JSON.stringify({
 task_id:id,
 status:newStatus
 })
+
 })
 
 }
